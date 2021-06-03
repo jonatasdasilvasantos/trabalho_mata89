ffe978d2f9dfcf6685f66f495ce23ffb4ae029c8
Jônatas da Silva <jonatass@ufba.br>
2021-06-03 15:21:14 -0300

    cria rotas
    Crias algumas rotas da API

--- backend/api/server.js
+++ backend/api/server.js
@@ -13,6 +13,33 @@ app.get('/main', (request, response) => {
     return response.status(200).json({ message: "main started!" });
 });
 
+app.get('/receitas', (request, response) => {
+    return response.status(200).json({ message: "receitas started!" });
+});
+
+app.get('/dashboard', (request, response) => {
+    return response.status(200).json({ message: "dashboard started!" });
+});
+
+app.get('/despesas', (request, response) => {
+    return response.status(200).json({ message: "despesas started!" });
+});
+
+app.post('/login', (request, response) => {
+    const { name, pass } = request.body;
+    console.log("User: " + name + ", password: " + pass);
+    return response.status(204).json({ message: "login started!", name: name, password: pass });
+});
+
+app.post('/cadastro', (request, response) => {
+    const { login, password, retryPassword, universidade, type } = request.body;
+    return response.status(201).json({ message: "cadastro started!", login: login, password: password, universidade: universidade, type: type });
+});
+
+app.put('/restore', (request, response) => {
+    return response.status(200).json({ message: "restore started!" });
+});
+
 app.listen(8546, () => {
     console.log('Olá Dev 👨‍🚀, embarque na nossa nave 🚀 e vamos para a base espacial, onde estão os marcianos 🛸!');
 });
