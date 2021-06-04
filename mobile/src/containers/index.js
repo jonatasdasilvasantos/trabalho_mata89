a04c34a66dd19abc70fa68b797ba53ef4d89c45c
Jônatas da Silva <jonatass@ufba.br>
2021-06-04 20:33:20 -0300

    insere novas rotas

--- mobile/src/containers/index.js
+++ mobile/src/containers/index.js
@@ -3,9 +3,11 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 // Screens
-import Main from './Main'
-import Home from './Home'
-import Dashboard from './Dashboard'
+import Main from './Main';
+import Home from './Home';
+import Login from './Login';
+import Cadastrar from './Cadastrar';
+import Dashboard from './Dashboard';
 
 const Stack = createStackNavigator()
 
@@ -14,6 +16,8 @@ function Routes() {
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
         <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastrar" component={Cadastrar} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
         {/* <Stack.Screen name="Home" component={Home} /> */}
       </Stack.Navigator>
